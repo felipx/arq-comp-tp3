b2db949559a5d8b13000107bd603f5472045ad35
felipx <felipemonterobruni@gmail.com>
2024-07-13 23:51:29 -0300

    feat: regfile - add integer regfile

--- cpu_core.v
+++ cpu_core.v
@@ -7,25 +7,26 @@
 module cpu_core
 #(
     // PC Parameters   
-    parameter NB_PC = 32,   //! NB of Program Counter
-
+    parameter NB_PC = 32,              //! NB of Program Counter
+    
     // Instruction Memory Parameters
-    parameter IMEM_ADDR_WIDTH = 10,
-    parameter IMEM_DATA_WIDTH = 32
-
+    parameter IMEM_ADDR_WIDTH = 10,    //! Instruction Memory address width
+    parameter IMEM_DATA_WIDTH = 32     //! Size of each memory location
+    
 ) (
     // outputs
     input i_rst,
     input clk
 );
-
+    
     //! Internal Signals
-    wire [NB_PC - 1 : 0] pc_out_connect;
-    wire [NB_PC - 1 : 0] adder_to_mux2to1;
-    wire [NB_PC - 1 : 0] mux2to1_to_pc;
-
-
-    // PC Adder
+    wire [NB_PC           - 1 : 0] pc_out_connect   ;  //! Program Counter output connection
+    wire [NB_PC           - 1 : 0] adder_to_mux2to1 ;  //! Program Counter's adder to mux connection
+    wire [NB_PC           - 1 : 0] mux2to1_to_pc    ;  //! Mux to Program Counter connection
+    wire [IMEM_DATA_WIDTH - 1 : 0] imem_to_if_id_reg;  //! Instruction memory to IF/ID reg connection
+    
+    
+    // PC's Adder
     adder
     #(
         .NB_ADDER (NB_PC)  //! NB of PC
@@ -37,8 +38,7 @@ module cpu_core
             .i_b   (32'h4               )   //! PC increments by 4
 
         );
-
-
+    
     // PC's Mux
     mux_2to1
     #(
@@ -52,7 +52,6 @@ module cpu_core
             .i_sel (                )   //TODO: connect signal
         );
     
-
     // Program Counter
     pc
     #(
@@ -66,7 +65,7 @@ module cpu_core
             .i_rst (i_rst               ),
             .clk   (clk                 )
         );
-
+    
     // Instruction Memory
     memory
     #(
@@ -75,7 +74,7 @@ module cpu_core
     )
         u_instruction_memory
         (
-            .o_dout (                                       ),  //! TODO: connect to IF/ID reg
+            .o_dout (imem_to_if_id_reg                      ),
             .i_din  (                                       ),  //! TODO: connection from uart
             .i_addr (pc_out_connect[IMEM_ADDR_WIDTH - 1 : 0]),  //! Truncate the address to fit the memory's address width
             .i_wen  (                                       ),  //! TODO: check where to connect
@@ -84,4 +83,39 @@ module cpu_core
             .clk    (clk                                    ) 
         );
     
+    // IF/ID Pipeline Register
+    if_id_reg
+    #(
+        .NB_INSTR (IMEM_DATA_WIDTH),
+        .NB_PC    (NB_PC          )
+    )
+        u_if_id_reg
+        (
+            .o_instr (                 ),  //! TODO
+            .o_pc    (                 ),  //! TODO
+            .i_instr (imem_to_if_id_reg),  
+            .i_pc    (pc_out_connect   ),   
+            .i_en    (1'b1             ),  //! TODO: Check if OK  
+            .i_rst   (i_rst            ),  
+            .clk     (clk              ) 
+        );
+
+    // Integer Register File
+    regfile
+    #(
+        .ADDR_WIDTH (),
+        .DATA_WIDTH ()
+    )
+        u_integer_regfile
+        (
+            .o_dout1 (),
+            .o_dout2 (),
+            .i_addr1 (),
+            .i_addr2 (),
+            .i_waddr (),
+            .i_wdata (),
+            .i_wen   (),
+            .i_rst   (),
+            .clk     () 
+        )
 endmodule
